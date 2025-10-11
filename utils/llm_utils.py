eab3d3313bbc1d14b379d88535bcfe76b70d8a68
huynhnc203 <hnc203204@gmail.com>
2025-10-11 09:40:13 +0700

    feat: llm module

---
+++ utils/llm_utils.py
@@ -0,0 +1,138 @@
+from abc import ABC, abstractmethod
+
+class LLM(ABC):
+    
+    def __init__(self, **args):
+        self.name = args["model_name"]
+    
+    @abstractmethod
+    def chat(context: str, **args):
+        pass
+    
+
+class FreeModel(LLM):
+    def __init__(self, **args):
+        pass
+        
+
+
+class CostModel(LLM):
+    def __init__(self, **args):
+        super().__init__(**args)
+        if args["model_name"].count("gpt"):
+            from openai import OpenAI
+            self.instance = OpenAI(api_key=args["API_KEY"])
+        elif args["model_name"].count("gemini"):
+            from google import genai
+            self.instance = genai.Client()
+        
+        self.system_prompt = args["system_prompt"]
+        if "context_template" in args: 
+            self.context_template = args["context_template"]
+            
+    def chat(self, context: str, json_return: bool = False, **args):
+        response: str = ""
+        
+        if self.name.count("gpt"):
+            try:
+                if self.context_template:
+                    resp = self.instance.responses.create(
+                        model = self.name,
+                        input = [
+                            {"role": "system", "content" : self.system_prompt},
+                            {"role": "user", "content" : self.context_template.format(context = context)}
+                        ]
+                    )
+                else:
+                    resp = self.instance.response.create(
+                        model = self.name,
+                        input = [
+                            {"role": "system", "content" : self.system_prompt},
+                            {"role": "user", "content" : context}
+                        ]
+                    )
+            except Exception as e:
+                logging.exception(f"Message in gpt response: {e}")
+                raise Exception(f"GPT problem {e}")
+            response = resp.output_text
+                
+        if self.name.count("gemini"):
+            from google.genai.types import GenerateContentConfig
+            try:
+                if self.context_template:
+                    resp = self.instance.models.generate_content(
+                        model = self.name,
+                        contents = self.context_template.format(context = context), 
+                        config=GenerateContentConfig(
+                            system_instruction=self.system_prompt
+                        )
+                    )
+                else:
+                    resp = self.instance.models.generate_content(
+                        model = self.name,
+                        contents = context, 
+                        config=GenerateContentConfig(
+                            system_instruction=self.system_prompt
+                        )
+                    )
+                response = resp.text
+            except Exception as e:
+                logging.exception(f"Message in Gemini response: {e}")
+                raise Exception(f"Gemini problem {e}")
+    
+        
+        if json_return:
+            response = json_valid(response)
+            
+        return response
+        
+        
+def json_valid(raw_resp: str) -> str:
+    raw_resp = raw_resp.strip("`").replace("json", "").strip()
+    return raw_resp
+
+
+if __name__ == "__main__":
+    from dotenv import load_dotenv
+    import logging
+    import os
+    
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        datefmt="%H:%M:%S"
+    )
+    logging.info("Start logging with time format")
+    load_dotenv()
+    
+    # config = {
+    #     "model_name": "gpt-3.5-turbo",
+    #     "API_KEY": os.environ["OPENAI"],
+    #     "system_prompt": "You are my assistance.",
+    #     "context_template": "Hello {context}"
+    # }
+    
+    # Test 1
+    # llm = CostModel(**config)
+    
+    # print(llm.chat(
+    #     "Huynh",
+    # ))
+    
+    
+    # Test 2
+    
+    config = {
+        "model_name": "gpt-3.5-turbo",
+        "API_KEY": os.environ["OPENAI"],
+        "system_prompt": "You are my assistance. Just return JSON format by my context I give you",
+        "context_template": "{context}"
+    }
+    
+    gemini = CostModel(**config)
+    print(gemini.chat(
+        "My name is Huynh. I am a student in VNU.", json_return=True
+    ))
+    
+    # Test 3
+               
